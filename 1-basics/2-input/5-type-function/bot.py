9001401d89c4db1a37ca508432a330da90c1eaa0
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-09-26 18:54:18 +0100

    Create 5-type-function/bot.py
    Added code to demonstrate the use of the type function and how data types can change as a result of operations.

---
+++ 1-basics/2-input/5-type-function/bot.py
@@ -0,0 +1,14 @@
+# Check type of data
+first_value = 30.5
+second_value = 5
+
+print(first_value, "is of type", type(first_value))
+print(second_value, "is of type", type(second_value))
+
+total = first_value + second_value
+print(total)
+
+print("Please enter a letter:")
+string = input()
+
+print(string, "is of type", type(string))
