c88bdbf01fdb821f1ec7194071054ad57fbd90b1
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-09-26 16:56:27 +0100

    Create 3-escape-characters/bot.py
    Added code to demonstrate the use of escape characters.

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,8 @@
+# Displaying escape characters
+print("\n Display a new line.")
+print("\t Display a tab space.")
+print("\\ Display a back slash.")
+print("\" Display a double quote.")
+print("\' Display a single quote.")
+
+print("\n \t \"It does not matter how slowly you go as long as you do not stop.\" – Confucius")
