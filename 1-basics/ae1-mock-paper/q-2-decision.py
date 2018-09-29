c5e345f447cd24d00fa995b03d3f72f2f37b8b65
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-09-29 12:54:07 +0100

    Create q-2-decision.py

---
+++ 1-basics/ae1-mock-paper/q-2-decision.py
@@ -0,0 +1,9 @@
+# Q2) Decision
+
+print("Please enter a whole number:")
+num = int(input())
+# an if statement that divides the input number by 2 and checks to see if it has a remainder; if so it is odd.
+if num % 2 == 0:
+  print(num, "is even")
+else:
+  print(num, "is odd")
