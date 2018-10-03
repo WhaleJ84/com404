be8fbaf6b14ffbfb1445af75ab17914d8010cf93
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-03 17:10:52 +0100

    Create bot.py
    Added code to demonstrate the use of the modulo operator with decisions.

---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,9 @@
+# Read a whole number from user
+print("Please enter a whole number:")
+user_number = int(input())
+
+# Work out if the number is even
+if user_number % 2 == 0:
+  print(user_number, "is even.")
+else:
+  print(user_number, "is odd.")
