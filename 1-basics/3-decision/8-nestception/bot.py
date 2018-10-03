893ac767fd78d6ee945076ab009498fd47133ffd
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-03 18:08:39 +0100

    Update bot.py

--- 1-basics/3-decision/8-nestception/bot.py
+++ 1-basics/3-decision/8-nestception/bot.py
@@ -7,27 +7,27 @@ print("In the lab")
 
 look = input()
 
-if look == ("In the bedroom"):
+if (look == "In the bedroom"):
   print("In the bedroom you see something under the bed.")
   print("Where should I look?")
   bedroom_look = input()
-  if bedroom_look == ("Under the bed"):
+  if (bedroom_look == "Under the bed"):
     print("Found some socks but no battery.")
   else:
     print("Found some mess but no battery.")
-elif look == ("In the bathroom"):
+elif (look == "In the bathroom"):
   print("In the bathroom you see something under the bathtub.")
   print("Where should I look?")
   bathroom_look = input()
-  if bathroom_look == ("Under the bathtub"):
+  if (bathroom_look == "Under the bathtub"):
     print("Found a rubber duck but no battery.")
   else:
     print("It's wet and there is no battery.")
-elif look == ("In the lab"):
+elif (look == "In the lab"):
   print("In the lab you see something on the table.")
   print("Where should I look?")
   lab_look = input()
-  if lab_look == ("On the table"):
+  if (lab_look == "On the table"):
     print("Found the battery!")
   else:
     print("Found some tools but no battery.")
