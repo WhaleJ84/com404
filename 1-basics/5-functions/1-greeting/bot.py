cc1004e5246fe0c4ceaccd668a958a1fd96f7fab
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-17 15:25:25 +0100

    Create bot.py
    Added code to display a greeting using a user-defined function.

---
+++ 1-basics/5-functions/1-greeting/bot.py
@@ -0,0 +1,10 @@
+# Creates a function to ask for your name
+def greet_user():
+  print("Please enter your name")
+  name = str(input())
+  print("Hello", name)
+
+# Runs the function 3 times
+greet_user()
+greet_user()
+greet_user()
