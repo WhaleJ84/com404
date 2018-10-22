2099affcb9c0c3c66421570c6aa80106c730d44d
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-22 18:39:18 +0100

    Update bot.py

--- 1-basics/5-functions/9-word-manipulation/bot.py
+++ 1-basics/5-functions/9-word-manipulation/bot.py
@@ -1,7 +1,8 @@
 def display_box(word):
   print("*" * (len(word) + 4))
   print("*", word, "*")
-  print("*" * (len(word) + 4)) 
+  print("*" * (len(word) + 4), end="") 
+  return("")
 
 def display_lower(word):
   lower_text = (word.lower())
@@ -12,37 +13,45 @@ def display_upper(word):
   return(upper_text)
 
 def display_mirror(word):
-  print(word, end="")
-  reverseWord = str()
+  print(word, "| ", end="")
   for position in range(len(word) -1, -1, -1):
     print(word[position], end="")
+  return("")
 
-#def display_repeat(word):
+def display_repeat(word):
+  print("How many times would you like to display the word?")
+  LOOP = int(input())
+  for count in range(LOOP):
+    if count % 2 == 0:
+      print(word.lower(), end="")
+    else:
+      print(word.upper(), end="")
+  return("")
 
-#print("Please enter a word:")
-#word = input()
-#print(
-#  """Please choose one of the following options:
-#  Display in a box
-#  Display lower-case
-#  Display upper-case
-#  Display mirrored
-#  Repeat
-#  """)
-#option = input()
+def run():
+  print("Please enter a word:")
+  word = input()
+  print(
+  """Please choose one of the following options:
+  Display in a box
+  Display lower-case
+  Display upper-case
+  Display mirrored
+  Repeat
+  """)
+  option = input()
 
-word = "TeStWoRd"
-option = "Display mirrored"
+  if option == "Display in a box":
+    print(display_box(word))
+  elif option == "Display lower-case":
+    print(display_lower(word))
+  elif option == "Display upper-case":
+    print(display_upper(word))
+  elif option == "Display mirrored":
+    print(display_mirror(word))
+  elif option == "Repeat":
+    print(display_repeat(word))
+  else:
+    print("Error.")
 
-if option == "Display in a box":
-  print(display_box(word))
-elif option == "Display lower-case":
-  print(display_lower(word))
-elif option == "Display upper-case":
-  print(display_upper(word))
-elif option == "Display mirrored":
-  print(display_mirror(word))
-elif option == "Repeat":
-  print(display_repeat(word))
-else:
-  print("Error.")
+run()
