7cb32fa9e645765bcd3c57a32001dcf9465b8580
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-31 15:42:11 +0000

    Create bot.py
    Added code to create a basic class and object.

---
+++ 2-guis/1-classes-and-objects/bot.py
@@ -0,0 +1,25 @@
+class Bot:
+
+  def __init__(self, name):
+    self.name = name
+    self.age = 0
+    self.energy = 100
+    self. shield = 100
+
+  def display_name(self):
+    print(self.name)
+
+  def display_age(self):
+    print(self.age)
+
+  def display_energy(self):
+    print(self.energy)
+
+  def display_shield(self):
+    print(self.shield)
+
+  def display_summary(self):
+    print("{} is {} years old and has {} energy and {} shield remaining.".format(self.name,self.age,self.energy,self.shield))
+
+  def __str__(self):
+    return("{} is {} years old and has {} energy and {} shield remaining.".format(self.name,self.age,self.energy,self.shield))
