3a0a626d987b3ef80383b9f0f21c47b23040ea60
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-31 18:49:41 +0000

    Update bot.py
    Added code to demonstrate inheritance.

--- 2-guis/1-classes-and-objects/bot.py
+++ 2-guis/1-classes-and-objects/bot.py
@@ -1,25 +1,49 @@
 class Bot:
 
-  def __init__(self, name):
+  def __init__(self, name, age=0, energy=100,shield=100):
     self.name = name
-    self.age = 0
-    self.energy = 100
-    self. shield = 100
+    self.age = age
+    self.energy = energy
+    self.shield = shield
+
+  def get_age(self):
+    return self.age
+
+  def get_energy(self):
+    return self.energy
+
+  def get_name(self):
+    return self.name 
+
+  def get_shield(self):
+    return self.shield
+
+  def decrement_energy(energy_decrease):
+    energy = (self.energy - energy_decrease)
+
+  def decrement_shield(shield_decrease):
+    shield = (self.shield - shield_decrease)
 
   def display_name(self):
-    print(self.name)
+    print("The robot's name is: {}".format(self.name))
 
   def display_age(self):
-    print(self.age)
+    print("The robot's age is: {}".format(self.age))
 
   def display_energy(self):
-    print(self.energy)
+    print("The robot's energy is: {}".format(self.energy))
 
   def display_shield(self):
-    print(self.shield)
+    print("The robot's shield is: {}".format(self.shield))
 
-  def display_summary(self):
-    print("{} is {} years old and has {} energy and {} shield remaining.".format(self.name,self.age,self.energy,self.shield))
+  def increment_age(self, age_increase):
+    age = (self.age + age_increase)
 
-  def __str__(self):
-    return("{} is {} years old and has {} energy and {} shield remaining.".format(self.name,self.age,self.energy,self.shield))
+  def increment_energy(self, energy_increase):
+    energy = (self.energy + energy_increase)
+
+  def incrememnt_shield(self, shield_increase):
+    shield = (self.shield + shield_increase)
+
+  def set_name(self, name):
+    self.name = (name)
