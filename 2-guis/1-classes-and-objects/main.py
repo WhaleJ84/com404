980f109bc137d973d5ca9e1f5cdf4e5eadfc0d69
WhaleJ84 <43613409+WhaleJ84@users.noreply.github.com>
2018-10-31 18:49:15 +0000

    Update main.py
    Added code to demonstrate inheritance.

--- 2-guis/1-classes-and-objects/main.py
+++ 2-guis/1-classes-and-objects/main.py
@@ -1,4 +1,6 @@
 from bot import Bot
+from flying_bot import FlyingBot
+from super_bot import SuperBot
 
 walle = Bot("WallE")
 
@@ -6,4 +8,14 @@ walle.display_name()
 walle.display_age()
 walle.display_energy()
 walle.display_shield()
-walle.display_summary()
+print()
+
+rob = SuperBot("ROB")
+
+print("The {}'s power level is: {}".format(rob.get_name(), rob.get_super_power_level()))
+print()
+
+alex = FlyingBot("Alex Murphy")
+
+print("{}'s hover distance is: {}".format(alex.get_name(), alex.get_hover_distance()))
+print()
